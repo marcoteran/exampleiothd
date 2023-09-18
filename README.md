c9cb8d75cf59309348246415ed3ac618994ec202
Marco Teran <marco.tulio.teran@gmail.com>
2023-09-18 17:10:08 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,10 @@
+# Example de IoT y GitHub
+
+Realizado por los estudiantes:
+- Josh
+- Daniel
+- Juan Camilo
+- Cesar
+
+Trabajo en equipo.
+
---
+++ sensoriot.ino
@@ -0,0 +1,43 @@
+// ARCHIVO SENSOR IOT
+
+// Declaración de Variables
+// Internet
+
+// DHT
+
+// Otras
+
+
+// Configuration
+void setup() {
+	// Serial configuration (JUAN CAMILO)
+
+
+	// DHT configuration (JOSH)
+
+
+	// WiFi configuration (Cesar)
+
+
+}
+
+
+
+
+// Loop de polling
+void loop() {
+	// Código de lectura de los sensores (JOSH)
+
+	t =
+	h = 
+	// Encienda y apague un LED (Daniel)
+
+
+	// Mostrar datos por serial (Cesar) - t,h
+  
+}
+
+
+// Funcion Leer datos del DHT 11 (JOSH)
+
+// Funcion Encender y apagar el LED (Daniel)
